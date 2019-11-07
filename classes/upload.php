2e0121b445e6b7bc77cb1464ccb68fd15489b88c
Thorben Auer <admin@mikropi.de>
2019-11-07 15:29:26 +0100

    Upload Fix

--- classes/upload.php
+++ classes/upload.php
@@ -1,12 +1,12 @@
 <?php
-
+ 
 if(!empty($_FILES)) {
     $files = $_FILES['files'];
-    if(file_exists("../files/tmp")){
-        mkdir("files/tmp");
+    if(!file_exists("../files/tmp")){
+        mkdir("../files/tmp");
     }
     for($i = 0; $i < count($files); $i++) {
-        move_uploaded_file($files['tmp_name'], "files/tmp/".$files['name']); //Upload the file
+        move_uploaded_file($files['tmp_name'], "../files/tmp/".$files['name']); //Upload the file
     }
 }
 
